45b5b86630c2e6a3106c0df087a713b4d4bada3a
Chris Debrah <chris.debrah@yahoo.com>
2020-09-21 19:14:17 +0000

    Change version number

--- src/Register/register.php
+++ src/Register/register.php
@@ -3,6 +3,6 @@
 use Console\App\Commands\CreateProjectCommand;
 use Symfony\Component\Console\Application;
 
-$app =  new Symfony\Component\Console\Application('GetInnotized Installer', '1.0.1');
+$app =  new Symfony\Component\Console\Application('GetInnotized Installer', '1.0.2');
 $app->add(new CreateProjectCommand());
 $app->run();
